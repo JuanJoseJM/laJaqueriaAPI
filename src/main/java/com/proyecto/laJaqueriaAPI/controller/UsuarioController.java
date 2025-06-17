f102b20091fd39583c909f209f913dc19e1dff35
JuanJoseJM <151637357+JuanJoseJM@users.noreply.github.com>
2025-06-17 11:20:38 +0200

    Anotación directa

--- src/main/java/com/proyecto/laJaqueriaAPI/controller/UsuarioController.java
+++ src/main/java/com/proyecto/laJaqueriaAPI/controller/UsuarioController.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
  */
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsuarioController {
 
     private final UsuarioService service;
